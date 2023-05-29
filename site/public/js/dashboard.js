292a5bf14adc4f74f02c2de757f6c211074d41f4
Kauan <kauaanmatheus@gmail.com>
2023-05-29 18:36:05 -0300

    Grafico de ocupacao geral funcionando

--- site/public/js/dashboard.js
+++ site/public/js/dashboard.js
@@ -16,13 +16,13 @@ document.addEventListener('DOMContentLoaded', () => {
         headers: {
             "Content-Type": "application/json"
         }
-    }).then(resposta=>{
-        if(resposta.ok){
-            resposta.json().then(json=>{
-                json.forEach(setor=>{
+    }).then(resposta => {
+        if (resposta.ok) {
+            resposta.json().then(json => {
+                json.forEach(setor => {
                     setores.push(setor)
-                    
-                    capacidadeMaxima.push({setor: setor.nome, capacidade: setor.capacidadeMaxima})
+
+                    capacidadeMaxima.push({ setor: setor.nome, capacidade: setor.capacidadeMaxima })
                 })
             })
         }
@@ -54,12 +54,12 @@ function buscarMedidas() {
     }).then((resposta) => {
         if (resposta.ok) {
             resposta.json().then(json => {
-                if(primeiroPlot){
+                if (primeiroPlot) {
                     json.forEach((element, index) => {
                         ocupacaoSetores.push(element.ocupacao)
                     });
-                }else{
-                    json.forEach((element, index) =>{
+                } else {
+                    json.forEach((element, index) => {
                         ocupacaoSetores.splice(index, 1, element.ocupacao)
                         ultimaAttdata.innerText = element.dtUltimaOcupacao
                     })
@@ -78,10 +78,16 @@ function buscarMedidas() {
     }).then((resposta) => {
         if (resposta.ok) {
             resposta.json().then(json => {
-                json.forEach(element => {
-                    ocupacaoGeral.push(element.ocupacao)
-                    historicoAtualizacao.push(element.data)
-                })
+                if (ocupacaoGeral.length < 6) {
+                    ocupacaoGeral.push(json[0].ocupacao)
+                    historicoAtualizacao.push(json[0].data)
+                } else {
+                    ocupacaoGeral.shift()
+                    historicoAtualizacao.shift()
+
+                    ocupacaoGeral.push(json[0].ocupacao)
+                    historicoAtualizacao.push(json[0].data)
+                }
             })
         }
     })
@@ -93,25 +99,25 @@ function buscarMedidas() {
 
 let graficoOcupacaoGeral, graficoVagaspSetor
 
-function loadCharts(){
-    let p40 = (capacidadeMaxima.capacidade * 40)/100
-    let p25 = (capacidadeMaxima.capacidade * 25)/100
-    let p75 = (capacidadeMaxima.capacidade * 75)/100
-    let p60 = (capacidadeMaxima.capacidade * 60)/100
+function loadCharts() {
+    let p40 = (capacidadeMaxima.capacidade * 40) / 100
+    let p25 = (capacidadeMaxima.capacidade * 25) / 100
+    let p75 = (capacidadeMaxima.capacidade * 75) / 100
+    let p60 = (capacidadeMaxima.capacidade * 60) / 100
 
-    ocupacaoSetores.forEach((ocupacao, index)=>{
-        if(ocupacao > p40 && ocupacao <= p60){
+    ocupacaoSetores.forEach((ocupacao, index) => {
+        if (ocupacao > p40 && ocupacao <= p60) {
             coresOcupacao[index] = 'green'
-        }else if(ocupacao <= p75 || ocupacao >= p25){
+        } else if (ocupacao <= p75 || ocupacao >= p25) {
             coresOcupacao[index] = 'orange'
-        }else{
-            coresOcupacao[index] = 'red'   
+        } else {
+            coresOcupacao[index] = 'red'
         }
     })
 
     if (!primeiroPlot) {
         attGraficos()
-    }else{
+    } else {
         graficoVagaspSetor = new Chart(chartVagaspSetor, {
             type: 'bar',
             data: {
@@ -148,20 +154,20 @@ function loadCharts(){
         graficoOcupacaoGeral = new Chart(chartOcupacaoGeral, {
             type: 'line',
             data: {
-                labels: [historicoAtualizacao[0]],
+                labels: [historicoAtualizacao[0], historicoAtualizacao[1], historicoAtualizacao[2], historicoAtualizacao[3], historicoAtualizacao[4], historicoAtualizacao[5]],
                 datasets: [{
                     label: 'Ocupação',
                     backgroundColor: [
                         'red'
                     ],
-                    data: [1],
+                    data: [ocupacaoGeral[0], ocupacaoGeral[1], ocupacaoGeral[2], ocupacaoGeral[3], ocupacaoGeral[4], ocupacaoGeral[5]],
                 }]
             },
             options: {
                 plugins: {
                     title: {
                         display: true,
-                        text: 'Ocupação de vagas por setores',
+                        text: 'Ocupação geral do estacionamento',
                         font: {
                             size: 17
 
@@ -184,13 +190,23 @@ function loadCharts(){
     }, 1000);
 }
 
-let dataVagaspSetor
+let dataVagaspSetor, dataOcupacaoGeral, labelsOcupacaoGeral
 function attGraficos() {
     dataVagaspSetor = graficoVagaspSetor.data.datasets[0].data
-    
+
     dataVagaspSetor.forEach((data, index) => {
         dataVagaspSetor[index] = ocupacaoSetores[index]
     })
 
     graficoVagaspSetor.update()
+
+    dataOcupacaoGeral = graficoOcupacaoGeral.data.datasets[0].data
+    labelsOcupacaoGeral = graficoOcupacaoGeral.data.labels
+
+    dataOcupacaoGeral.forEach((data, index) => {
+        dataOcupacaoGeral[index] = ocupacaoGeral[index]
+        labelsOcupacaoGeral[index] = historicoAtualizacao[index]
+    })
+
+    graficoOcupacaoGeral.update()
 }
